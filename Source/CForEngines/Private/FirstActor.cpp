05f55a39d624794438481dc97a86e42f72c201b8
BenHoldway03 <benholdway03@gmail.com>
2024-09-28 22:17:05 +0100

    Initial commit

---
+++ Source/CForEngines/Private/FirstActor.cpp
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FirstActor.h"
+
+
+// Sets default values
+AFirstActor::AFirstActor()
+{
+	_Root = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+	RootComponent = _Root;
+
+	_Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	_Mesh -> SetupAttachment(_Root);
+
+	_Arrow = CreateDefaultSubobject<UArrowComponent>(TEXT("Arrow"));
+	_Arrow -> SetupAttachment(_Root);
+	
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void AFirstActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AFirstActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
---
+++ Source/CForEngines/Private/FirstActor.h
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ArrowComponent.h"
+#include "GameFramework/Actor.h"
+#include "FirstActor.generated.h"
+
+UCLASS()
+class CFORENGINES_API AFirstActor : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AFirstActor();
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	TObjectPtr<USceneComponent> _Root;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	TObjectPtr<UStaticMeshComponent> _Mesh;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	TObjectPtr<UArrowComponent> _Arrow;
+	
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+};
