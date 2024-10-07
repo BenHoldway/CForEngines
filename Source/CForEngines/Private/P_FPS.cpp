1a4c1c1e72abff8a4b9f3e5a7fa29e2b9690aa7b
Ben Holdway <93982251+BenHoldway@users.noreply.github.com>
2024-10-07 12:05:28 +0100

    Session 3 - C++ Input Actions

---
+++ Source/CForEngines/Private/P_FPS.cpp
@@ -0,0 +1,54 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "P_FPS.h"
+
+#include "HealthComponent.h"
+#include "Camera/CameraComponent.h"
+
+
+// Sets default values
+AP_FPS::AP_FPS()
+{
+	_Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	_Camera->SetupAttachment(RootComponent);
+	_Health = CreateDefaultSubobject<UHealthComponent>(TEXT("Health"));
+}
+
+void AP_FPS::Input_Move_Implementation(FVector2D value)
+{
+	AddMovementInput(FVector::VectorPlaneProject(_Camera->GetForwardVector(), FVector::UpVector).GetSafeNormal(), value.Y);
+	AddMovementInput(_Camera->GetRightVector(), value.X);
+}
+
+void AP_FPS::Input_Look_Implementation(FVector2D value)
+{
+	AddActorWorldRotation(FRotator(0.0f, value.X, 0.0f));
+	_Camera->AddLocalRotation(FRotator(value.Y, 0.0f, 0.0f));
+}
+
+void AP_FPS::Input_JumpPressed_Implementation()
+{
+	ACharacter::Jump();
+}
+
+void AP_FPS::Input_JumpReleased_Implementation()
+{
+	ACharacter::StopJumping();
+}
+
+void AP_FPS::Input_FirePressed_Implementation()
+{
+	//TODO Make fire
+}
+
+void AP_FPS::Input_FireReleased_Implementation()
+{
+	//TODO Stop fire
+}
+
+UInputMappingContext* AP_FPS::GetMappingContext_Implementation()
+{
+	return _MappingContext;
+}
+
